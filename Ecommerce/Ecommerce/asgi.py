12193fd9a98d3d3f15f7153c90ee36f9483c739f
nishant-singh21 <122474129+nishant-singh21@users.noreply.github.com>
2026-08-11 01:44:26 +0530

    reset

--- Ecommerce/Ecommerce/asgi.py
+++ Ecommerce/Ecommerce/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for My_Shop project.
+ASGI config for Ecommerce project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'My_Shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
 
 application = get_asgi_application()
--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for My_Shop project.
+URL configuration for Ecommerce project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password reset
-    path("", include("shop.urls")),
+    path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
