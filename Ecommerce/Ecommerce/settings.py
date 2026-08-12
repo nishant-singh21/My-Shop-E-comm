3344b926b515918c6ed8f5d5f403a7f35a3ed391
nishant-singh21 <122474129+nishant-singh21@users.noreply.github.com>
2026-08-12 16:29:42 +0530

    add the index and  static from bootstrap made
    And  load static

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -116,6 +116,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
+
+
+
+
 
 
 # Email
