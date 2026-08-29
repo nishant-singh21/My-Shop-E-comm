3c96adad960666c97ebbed5bc1cbe06f1c6454c4
Nishant Singh <122474129+nishant-singh21@users.noreply.github.com>
2026-08-30 01:22:42 +0530

    start now the login and signup

--- Ecommerce/authcart/urls.py
+++ Ecommerce/authcart/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from authcart import views 
 
 urlpatterns = [
-    path('Signup/', views.signup, name='signup'),
-    path('Login/', views.handlelogin, name='handlelogin'),
-    path('LogOut/', views.handlelogout, name='handlelogout'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.handlelogin, name='handlelogin'),
+    path('logout/', views.handlelogout, name='handlelogout'),
     
 
 ]
