d9266293cf09a5b98aa5b3ae1b5559b973a276c2
Nishant Singh <122474129+nishant-singh21@users.noreply.github.com>
2026-09-08 09:56:07 +0530

    Now working on the admin pannel

--- Ecommerce/authcart/views.py
+++ Ecommerce/authcart/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect ,HttpResponse
+
+from django.contrib.auth.models import User
 
 def signup(request):
+       if request.method=='POST':
+            email=request.POST['email']
+            password=request.POST['pass1']
+            confirm_password=request.POST['pass2']
+            if password!=confirm_password:
+                return HttpResponse("Password and confirm password are not same! Please try again")
+                # return render (request, "authentication/signup.html")
+
+            try: 
+                 if User.objects.get(username=email):
+                      return HttpResponse("Email is already registered! Please try again")
+                    # return render (request, 'authentication/signup.html')
+            
+               
+            except Exception as identifier:
+                 pass 
+            user= User.objects.create_user(email,email,password)
+            user.save()
+            return HttpResponse("Your account has been successfully created! Please login to continue")
+            
        return render (request, "authentication/signup.html")
 
 
-def handlelogin(request):
+def handlelogin(request): 
     return render (request, "authentication/login.html")
 
 def handlelogout(request):
